c999d6d76c0fa73e6c9115391311458be3a822ca
austinbrodeur <austinbrodeur@shaw.ca>
2017-02-27 18:28:05 -0700

    Updated menu/args
    Working within assignment specs. Main menu loop also working.

--- EArgs.java
+++ EArgs.java
@@ -7,7 +7,7 @@ public class EArgs
 
   private String helpText = "Synopsis:\n  methods\n  methods { -h | -? | --help }+\n  methods {-v --verbose}* <jar-file> [<class-name>]\nArguments:\n  <jar-file>:   The .jar file that contains the class to load (see next line).\n  <class-name>: The fully qualified class name containing public static command methods to call. [Default=\"Commands\"]\nQualifiers:\n  -v --verbose: Print out detailed errors, warning, and tracking.\n  -h -? --help: Print out a detailed help message.\nSingle-char qualifiers may be grouped; long qualifiers may be truncated to unique prefixes and are not case sensitive.";
   private String synoText = "\n\nThis program interprets commands of the format '(<method> {arg}*)' on the command line, finds corresponding\nmethods in <class-name>, and executes them, printing the result to sysout.";
-
+  private String menuText = "\nq           : Quit the program.\nv           : Toggle verbose mode (stack traces).\nf           : List all known functions.\n?           : Print this helpful text.\n<expression>: Evaluate the expression.\nExpressions can be integers, floats, strings (surrounded in double quotes) or function\ncalls of the form '(identifier {expression}*)'.";
 
   // Takes in list of command line arguments and performs error checking.
   public EArgs(String argsList[])
@@ -21,7 +21,14 @@ public class EArgs
     {
       System.out.println(helpText); // Prints program use and takes in no arguments
     }
-    else if ((argsList.length == 1) || (argsList.length == 2))
+    else if (argsList.length == 1)
+    {
+      if ((argsList[0].equals("-h")) || (argsList[1].equals("-?")) || (argsList[1].equals("--help"))) // If first arg is for help
+      {
+        System.out.println(helpText + synoText);
+      }
+    }
+    else if (argsList.length == 2)
     {
       if (!(argsList[0].endsWith(".jar"))) // First arg ends with .jar
       {
@@ -29,29 +36,34 @@ public class EArgs
         System.err.println("EXIT -3");
         System.exit(-3);
       }
-      else if ((argsList[0].equals("-h")) || (argsList[1].equals("-?")) || (argsList[1].equals("--help"))) // If first arg is for help
+      else if ((argsList[0].endsWith(".jar")))
       {
-        System.out.println(helpText + synoText);
-      }
-      else if (argsList[0].endsWith(".jar"))
-      {
-        // Check if .jar exists
-        // If yes, start with that jar. If no, error
-      }
-      else if (argsList.length == 3) // Checks for qualifiers
-      {
-        if ((argsList[0].equals("-v")) || (argsList[0].equals("--verbose"))) // For verbose mode
-        {
-          System.out.println("Verbose mode"); // Code for verbose mode here
-        }
+        // Check if .jar and .class exist
+        // If yes, start with that jar. If no, error (-5 exit code)
+        Menu();
       }
     }
+    else if (argsList.length == 3) // Checks for qualifiers
+    {
+      if ((argsList[0].equals("-v")) || (argsList[0].equals("--verbose"))) // For verbose mode
+      {
+        System.out.println("Verbose mode"); // Code for verbose mode here
+      }
+    }
+  }
+
+  public Boolean CheckJar()
+  {
+    // Will check if a .jar file exists
+    return true;
   }
 
   public void Menu()
   {
     do
     {
+      System.out.println(menuText);
+      System.out.print(">");
       String choice;
       choice = s.next();
 
@@ -59,22 +71,28 @@ public class EArgs
       {
         case "q":
         case "Q":
+        System.out.println("bye.");
+        System.exit(0);
         break;
 
         case "v":
         case "V":
+        // Toggle verbose
         break;
 
         case "f":
         case "F":
+
         break;
 
         case "?":
+        System.out.println(helpText + synoText);
         break;
 
         default:
+        System.out.println("Evaluate expression");
         break;
       }
-    } while (True);
+    } while (true);
   }
 }
